e96be441323d2b03f4f33539c93c7395a493ed56
tanvir-sec <2021338528@student.sec.ac.bd>
2025-08-24 12:39:24 +0600

    Practice

--- hello.py
+++ hello.py
@@ -1,15 +1,24 @@
-# import pandas as pd
+import matplotlib.pyplot as plt
 
-# print(pd.__version__)
+x1=[1,2,3,4,5]
+x2=[1,0,1,4,9]
+y1=[2,3,4,6,7]
+y2=[1,4,7,8,2]
+plt.figure(figsize=(10,5))
 
+plt.subplot(2,2,1)
+plt.plot(x1,y2,color='black',linestyle='dashdot')
+plt.title('plot 1')
 
-import pandas as pd
+plt.subplot(2,2,2)
+plt.plot(x2,y1,color='blue',linestyle='dashdot')
+plt.title('plot 2')
 
-mydataset = {
-  'cars': ["BMW", "Volvo", "Ford"],
-  'passings': [3, 7, 2]
-}
+plt.subplot(2,2,3)
+plt.plot(x2,y2,color='red',linestyle='dashdot')
+plt.title('plot 3')
 
-myvar = pd.DataFrame(mydataset)
-
-print(myvar)
+plt.subplot(2,2,4)
+plt.plot(x1,y1,color='black',linestyle='dashdot')
+plt.title('plot 4')
+plt.show()
