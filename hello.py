1279f44eae232023e9d7cf93a2dfb7798e80a91d
tanvir-sec <2021338528@student.sec.ac.bd>
2025-08-27 07:45:46 +0600

    Basic code

--- hello.py
+++ hello.py
@@ -1,24 +1,4 @@
-import matplotlib.pyplot as plt
-
-x1=[1,2,3,4,5]
-x2=[1,0,1,4,9]
-y1=[2,3,4,6,7]
-y2=[1,4,7,8,2]
-plt.figure(figsize=(10,5))
-
-plt.subplot(2,2,1)
-plt.plot(x1,y2,color='black',linestyle='dashdot')
-plt.title('plot 1')
-
-plt.subplot(2,2,2)
-plt.plot(x2,y1,color='blue',linestyle='dashdot')
-plt.title('plot 2')
-
-plt.subplot(2,2,3)
-plt.plot(x2,y2,color='red',linestyle='dashdot')
-plt.title('plot 3')
-
-plt.subplot(2,2,4)
-plt.plot(x1,y1,color='black',linestyle='dashdot')
-plt.title('plot 4')
-plt.show()
+import pandas as pd
+df=pd.read_csv('consumer data.csv')
+df.head()
+df.columns
